2283b8c78f3788129e092e76ad335ab52fe26f12
Pavel Kozaev <pkozaev221299@gmail.com>
2023-06-02 18:09:28 +0300

    Перенести службу книг на прикладной уровень

--- presentation/Store.Web/Controllers/OrderController.cs
+++ presentation/Store.Web/Controllers/OrderController.cs
@@ -109,7 +109,7 @@ namespace Store.Web.Controllers
             else
             {
                 order = orderRepository.Create();
-                cart = new Cart(order.Id);
+                cart = new Cart(order.Id, 0, 0m);
             }
 
             return (order, cart);
@@ -119,8 +119,7 @@ namespace Store.Web.Controllers
         {
             orderRepository.Update(order);
 
-            cart.TotalCount = order.TotalCount;
-            cart.TotalPrice = order.TotalPrice;
+            cart = new Cart(order.Id, order.TotalCount, order.TotalPrice);
 
             HttpContext.Session.Set(cart);
         }
