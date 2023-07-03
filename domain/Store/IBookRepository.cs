3d3b942b57587d149b83bb09b92acc2bf20ab1d7
Pavel Kozaev <pkozaev221299@gmail.com>
2023-07-03 23:12:51 +0300

    Сделать код асинхронным

--- domain/Store/IBookRepository.cs
+++ domain/Store/IBookRepository.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Store
 {
     public interface IBookRepository
     {
-        Book[] GetAllByIsbn(string isbn);
+        Task<Book[]> GetAllByIsbnAsync(string isbn);
 
-        Book[] GetAllByTitleOrAuthor(string titleOrAuthor);
+        Task<Book[]> GetAllByTitleOrAuthorAsync(string titleOrAuthor);
 
-        Book GetById(int id);
+        Task<Book> GetByIdAsync(int id);
 
-        Book[] GetAllByIds(IEnumerable<int> bookIds);
+        Task<Book[]> GetAllByIdsAsync(IEnumerable<int> bookIds);
     }
 }
